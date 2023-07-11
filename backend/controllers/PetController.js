d1ca231ce643aad10f726fa51d5b8b1a41fd7619
tcharlesfr <tcharlesandroid@gmail.com>
2023-07-11 12:31:11 -0300

    add: criação de pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,8 +1,63 @@
-const Pet = require('../models/Pet')
+const Pet = require("../models/Pet");
+
+//helpers
+const getToken = require("../helpers/get-token");
+const getUserByToken = require("../helpers/get-user-by-token");
 
 module.exports = class PetController {
-    //criar pet
-    static async create(req, res){
-        res.json({message: "create ok"})
+  //criar pet
+  static async create(req, res) {
+    const { name, age, weight, color } = req.body;
+
+    // setar como disponivel logo quando criado
+    const available = true;
+
+    // upload de imagens
+
+    //validações
+    if (!name) {
+      res.status(422).json({ message: "o nome é obrigatorio" });
+      return;
     }
-}
+    if (!age) {
+      res.status(422).json({ message: "a idade é obrigatoria" });
+      return;
+    }
+    if (!weight) {
+      res.status(422).json({ message: "o peso é obrigatorio" });
+      return;
+    }
+    if (!color) {
+      res.status(422).json({ message: "a cor é obrigatoria" });
+      return;
+    }
+
+    // pegando os dados do dono
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    // criando pet
+    const pet = new Pet({
+      name,
+      age,
+      weight,
+      color,
+      available,
+      images: [],
+      user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    });
+
+    // salvando no banco de dados
+    try {
+      const newPet = await pet.save();
+      res.status(201).json({ message: "pet criado com sucesso", newPet });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+};
--- backend/helpers/get-token.js
+++ backend/helpers/get-token.js
@@ -4,7 +4,6 @@
 const getToken = (req) => {
   const authHeader = req.headers.authorization;
   const token = authHeader.split(" ")[1];
-  console.log(token)
   return token;
 };
 
