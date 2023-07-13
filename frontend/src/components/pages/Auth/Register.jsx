f49a29fd4a476ce0813aa7f9ea8c373cd0a9412b
tcharlesfr <tcharlesandroid@gmail.com>
2023-07-12 22:23:47 -0300

    add: component input

--- frontend/src/components/pages/Auth/Register.jsx
+++ frontend/src/components/pages/Auth/Register.jsx
@@ -1,9 +1,50 @@
+import Input from "../../form/Input";
+
 function Register() {
-    return (
-      <section>
-        <h1>Register</h1>
-      </section>
-    );
-  }
-  
-  export default Register;
+  function handleChange(e) {}
+  return (
+    <section>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu email"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input type="submit" value="Cadastrar" />
+      </form>
+    </section>
+  );
+}
+
+export default Register;
