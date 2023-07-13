d449958b5292655d6cfec48d9546df90cce1e15b
tcharlesfr <tcharlesandroid@gmail.com>
2023-07-13 16:56:22 -0300

    finalizando o flash message e criação de usuario pelo frontend

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -3,18 +3,28 @@ import api from "../utils/api";
 
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useFlashMessage from "./useFlashMessage";
 
-export default function useAuth(){
-    async function register(user){
-        try {
-            const data = await api.post('/users/register', user).then((response) => {
-                return response(data)
-            })
-        } catch (error) {
-            //tratar erro
-            console.log(error)
-        }
+export default function useAuth() {
+    //disparador de mensagem
+  const { setFlashMessage } = useFlashMessage();
+
+  async function register(user) {
+    //mensagens de sucesso e tipo
+    let msgText = "Cadastro realizado com sucesso";
+    let msgType = "sucess";
+
+    try {
+      const data = await api.post("/users/register", user).then((response) => {
+        return response.data; //response(data)
+      });
+    } catch (error) {
+      //mensagens de sucesso e tipo
+      msgText = error.response.data.message;
+      msgType = "error";
     }
-    return {register}
-}
 
+    setFlashMessage(msgText, msgType);
+  }
+  return { register };
+}
