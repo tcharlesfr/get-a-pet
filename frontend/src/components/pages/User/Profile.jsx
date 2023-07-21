c510e7979c86b2a4a0f9b2d89b40544ff88f1f67
tcharlesfr <tcharlesandroid@gmail.com>
2023-07-21 16:18:23 -0300

    add: formulario de edição de usuario

--- frontend/src/components/pages/User/Profile.jsx
+++ frontend/src/components/pages/User/Profile.jsx
@@ -1,7 +1,72 @@
+import { useState, useEffect } from 'react';
+
+import styles from './Profile.module.css'
+import formStyles from '../../form/Form.module.css'
+import Input from '../../form/Input'
+
 function Profile() {
+  const [user, setUser] = useState({})
+
+  function onFileChange(e){
+    
+  }
+  function handleChange(e){
+    
+  }
     return (
       <section>
-        <h1>Profile</h1>
+        <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        <p>imagem</p>
+        </div>
+        <form action="" className={formStyles.form_container}>
+          <Input
+            text='Imagem'
+            type='file'
+            name='image'
+            handleOnChange={onFileChange}
+          />
+          <Input
+            text='E-mail'
+            type='email'
+            name='email'
+            placeholder='Digite o seu e-mail'
+            handleOnChange={handleChange}
+            value={user.email || ''}
+          />
+          <Input
+            text='Nome'
+            type='text'
+            name='name'
+            placeholder='Digite o seu nome'
+            handleOnChange={handleChange}
+            value={user.name || ''}
+          />
+          <Input
+            text='Telefone'
+            type='text'
+            name='phone'
+            placeholder='Digite o seu telefone'
+            handleOnChange={handleChange}
+            value={user.phone || ''}
+          />
+          <Input
+            text='Senha'
+            type='password'
+            name='password'
+            placeholder='Digite a sua senha'
+            handleOnChange={handleChange}
+          />
+          <Input
+            text='Confirmação de Senha'
+            type='password'
+            name='confirmpassword'
+            placeholder='confirme a sua senha'
+            handleOnChange={handleChange}            
+          />
+          <input type="submit" value='Editar'/>
+         
+        </form>
       </section>
     );
   }
