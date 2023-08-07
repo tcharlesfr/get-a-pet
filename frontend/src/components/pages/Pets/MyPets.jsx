57a9715ef47cf4ae5ccf029a445483ed9fca1368
tcharlesfr <tcharlesandroid@gmail.com>
2023-08-07 09:58:18 -0300

    add: pegando os pets no bd

--- frontend/src/components/pages/Pets/MyPets.jsx
+++ frontend/src/components/pages/Pets/MyPets.jsx
@@ -1,8 +1,29 @@
+import api from '../../../utils/api'
+
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import RoundedImage from '../../layout/RoundedImage'
+
+//hooks
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 function MyPets() {
   const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem('token') || '')
+  const {setFlashMessage} = useFlashMessage()
+
+  //chamar a api, enviando o token de autorização
+  useEffect(() => {
+    api.get('/pets/mypets', {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    })
+    .then((response) => {
+      setPets(response.data.pets)
+    })
+  }, [token])
 
   return (
     <section>
