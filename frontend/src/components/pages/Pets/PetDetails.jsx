762aeba36839a8611a096db4460b595355a08e61
tcharlesfr <tcharlesandroid@gmail.com>
2023-08-08 18:55:15 -0300

    estruturando a pagina de detalhes do pet

--- frontend/src/components/pages/Pets/PetDetails.jsx
+++ frontend/src/components/pages/Pets/PetDetails.jsx
@@ -9,10 +9,10 @@ import { useParams, Link } from "react-router-dom";
 import useFlashMessage from "../../../hooks/useFlashMessage";
 
 function PetDetails() {
-  const [ pet, setPet ] = useState({});
+  const [pet, setPet] = useState({});
   const { id } = useParams();
   const { setFlashMessage } = useFlashMessage();
-  const [ token ] = useState(localStorage.getItem("token") || "");
+  const [token] = useState(localStorage.getItem("token") || "");
 
   useEffect(() => {
     api.get(`/pets/${id}`).then((response) => {
@@ -20,7 +20,38 @@ function PetDetails() {
     });
   }, [id]);
 
-  return <h1>{pet.name}</h1>;
+  return (
+    <>
+      {pet.name && (
+        <section>
+          <div>
+            <h1>Conhecendo o Pet: {pet.name}</h1>
+            <p>Se tiver interesse, marque uma visita para conhecê-lo</p>
+          </div>
+          <div>
+            {pet.images.map((image, index) => (
+              <img
+                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                alt={pet.name}
+                key={index}
+              />
+            ))}
+          </div>
+          <p>
+            <span className="bold">Peso: </span>{pet.weight}kg
+          </p>
+          <p>
+            <span className="bold">Idade: </span>{pet.age} anos
+          </p>
+                {token ? (
+                    <button>solicitar uma visita</button>
+                ) : (
+                    <p>Você precisa <Link to='/register'>criar uma conta</Link> para solicitar uma visita</p>
+                )}
+        </section>
+      )}
+    </>
+  );
 }
 
 export default PetDetails;
